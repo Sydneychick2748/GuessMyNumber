666ef27279f32b9bc7f3594c45c338dde6bc66fe
ana <ana@anniecannons.com>
2022-08-16 10:25:22 -0700

    first commmit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+import Colors from '../../constants/colors';
+
+function NumberContainer({ children }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.numberText}>{children}</Text>
     </View>
   );
 }
 
+export default NumberContainer;
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    // fontWeight: 'bold',
+    fontFamily: 'open-sans-bold'
   },
 });
